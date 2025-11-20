0804c5e61e4a85e3a6f5155332cd954955b115ed
kenka <kenka@email>
2025-11-19 20:18:02 -0500

    version1.4

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://wrhgsuuzipsummbilxbg.supabase.co";
-const SUPABASE_ANON = "SUPABASE_CLIENT_API_KEY";
+const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyaGdzdXV6aXBzdW1tYmlseGJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1ODYxOTAsImV4cCI6MjA3OTE2MjE5MH0.VMARFRK6dE8F7HfdXkeqwHxmRskR7AYOHPpbOT-P3rM";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
 
